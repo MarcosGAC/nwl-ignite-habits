52e9d1a1872f2a5acae6fcadbd035199c870eb8e
Marcos Gabriel <125589443+MarcosGAC@users.noreply.github.com>
2023-02-17 11:08:38 -0300

    Update axios.ts

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
-
+//http://localhost:3333
 export const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'mysql://q1sr4a4p2vtc8ink1v3z:pscale_pw_hAUXZjbgkpTUQgdAPT9NpK84vJ0DHVEyv44I5Hnh3KC@us-east.connect.psdb.cloud/app?sslaccept=strict'
+    
 })
